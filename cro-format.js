e256c9fb90f0260b2ae12fa31a4cac0328be42ca
CroatiaParanoia <1369248927@qq.com>
2019-07-31 23:22:04 +0800

    新增 cro-format.js
    初版， 可以针对 对象，字符， 数字， 布尔， 普通数组进行格式化

---
+++ cro-format.js
@@ -0,0 +1,99 @@
+var CroFormat = (function() {
+  var ClassArr = [Number, String, Boolean, Array, Object];
+
+  function getDataType(data) {
+    const dataTypeMap = {
+      Number: Number,
+      String: String,
+      Boolean: Boolean,
+      Function: Function,
+      Array: Array,
+      Object: Object
+    };
+    return dataTypeMap[{}.toString.call(data).slice(8, -1)];
+  }
+
+  function transformData(value, dataType, normalValue) {
+    switch (dataType) {
+      case Number:
+        {
+          const tempRes = Number(value === null ? undefined : value);
+          return isNaN(tempRes) ? normalValue || 0 : tempRes;
+        }
+        break;
+      case String:
+        {
+          return String(value || normalValue || "");
+        }
+        break;
+      case Boolean:
+        {
+          return Boolean(value);
+        }
+        break;
+      case Array:
+        {
+          return getDataType(value) === Array ? value : normalValue || [];
+        }
+        break;
+      case Object:
+        {
+          return getDataType(value) === Object ? value : normalValue || {};
+        }
+        break;
+      default:
+    }
+  }
+
+  function formatItem(value, formatConf) {
+    const DT_value = getDataType(value);
+    const DT_formatConf = getDataType(formatConf);
+
+    // 如果是基本数据类型判断
+
+    // formatItem('123', [Number, 0]);
+    if (DT_formatConf === Array) {
+      // 此方式校验数据， 数组中第一个参数为 数据类型， 第二个参数为默认值;
+
+      if (formatConf.length && ClassArr.includes(formatConf[0])) {
+        return transformData(value, formatConf[0], formatConf[1]);
+      }
+
+      throw new Error("请添加要校验的数据类型");
+    }
+
+    // formatItem('123', Number);
+    if (ClassArr.includes(formatConf)) {
+      return transformData(value, formatConf);
+    }
+  }
+
+  function format(dataSource, formatConf) {
+    const DT_dataSource = getDataType(dataSource);
+    const DT_formatConf = getDataType(formatConf);
+
+    if (DT_formatConf === Object) {
+      let tempObj = {};
+      for (const key in formatConf) {
+        const tempItem = (dataSource || {})[key];
+        const tempFormatConf = formatConf[key];
+
+        const DT_tempItem = getDataType(tempItem);
+        const DT_tempFormatConf = getDataType(tempFormatConf);
+
+        if (DT_tempFormatConf === Object) {
+          tempObj[key] = format(tempItem, tempFormatConf);
+        } else {
+          tempObj[key] = formatItem(tempItem, tempFormatConf);
+        }
+      }
+
+      return tempObj;
+    } else {
+      return formatItem(dataSource, formatConf);
+    }
+  }
+
+  format.getDataType = getDataType;
+  return format;
+})();
